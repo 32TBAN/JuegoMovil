59b0e370c2e5f2d83a626cae3724dbd24251e018
Kevin Guanoluisa <estebanguanoluisa55@gmail.com>
2024-07-31 21:27:19 -0500

    feat($/scenes): iniciar desarrollo del escenario
    - Se comenzó el desarrollo del escenario principal del juego.
    - Se creó la estructura inicial de la escena y se añadieron elementos básicos como el fondo y el terreno.
    - Se configuraron los elementos para el diseño inicial, incluyendo posiciones y dimensiones básicas.

--- scripts/characters/enemies/eagle/egg.gd
+++ scripts/characters/enemies/eagle/egg.gd
@@ -1,14 +1,13 @@
 extends RigidBody2D
 
-@onready var game_manager = $"../../../GameManager"
+@onready var game_manager =  $"../../../../GameManager"
 
 func _on_area_2d_body_entered(body):
 	if body.name == "player1":
 		var x_delta = position.x - body.position.x
-		if x_delta < 2:
-			game_manager.decrease_health()
+		if x_delta < 2.6:
+			game_manager.decrease_health()	
 	get_node("AnimatedSprite2D").play("break")
-	get_node("Area2D/CollisionShape2D").disabled = true
 
 
 func _on_animated_sprite_2d_animation_finished():
