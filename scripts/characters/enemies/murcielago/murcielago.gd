cc4357e036f7513d06d7c72d81684c4b55472784
Kevin Guanoluisa <estebanguanoluisa55@gmail.com>
2024-12-10 10:19:40 -0500

    attack for enemys bats

--- scripts/characters/enemies/murcielago/murcielago.gd
+++ scripts/characters/enemies/murcielago/murcielago.gd
@@ -1,70 +1,99 @@
 extends CharacterBody2D
 
 const SPEED = 200.0
+const CLOSE_RANGE_X = 30  # Rango cercano en X
+const CLOSE_RANGE_Y = 60  # Rango cercano en Y
+const ATTACK_RANGE = 200  # Rango de ataque adicional
 
 var is_dead = false
+var is_area_player = false
+var was_attack = false
+
 @onready var anim = $AnimatedSprite2D
-var direction = Vector2(1, 0)
-@onready var player_camera = get_parent().get_parent().get_node("Player/player1/Camera2D")
-@onready var player = get_parent().get_parent().get_node("Player/player1")
+@onready var player_camera = get_parent().get_parent().get_parent().get_node("Player/player1/Camera2D")
+@onready var player = get_parent().get_parent().get_parent().get_node("Player/player1")
 
 var left_limit = 0
 var right_limit = 0
-
-var is_area_player = false
+var direction = Vector2(1, 0)
 
 func _ready():
-	
 	update_limits()
 	
 func _physics_process(delta):
 	if is_dead:
 		return
+	anim.play("fly")
 	if is_area_player:
-		velocity.x = 0
-		var direction_to_player = (player.position - position).normalized()
-		$AnimatedSprite2D.rotation = direction_to_player.angle()
-		#print(direction_to_player.x)
-		#print(position.x)
-		
-		if position.x > 0:
-			position.x -= 1
-		else:
-			position.x += 1
-		#if position != direction_to_player:
-			#while position.x != position.x:
-				#position.x += 100
-				#
-		#print("Dirección al jugador:", direction_to_player)
-		#print("Posición del murciélago:", position)
+		handle_player_interaction()
+	## si la altura del murcielago es mas que 
+	elif round(position.y) <= round(player.position.y-250):
+		position.y += 5
 	else:
 		move_enemy(delta)
 		update_limits()
 
+
+func handle_player_interaction():
+	anim.speed_scale = 2
+	# Calcular dirección hacia el jugador
+	var direction_to_player = (player.position - position).normalized()
+	anim.rotation = direction_to_player.angle() - 2
+		
+	var player_pos = Vector2(round(player.position.x), round(player.position.y))
+	var bat_pos = Vector2(round(position.x), round(position.y))
+
+	#if distance_x < CLOSE_RANGE_X and distance_y < CLOSE_RANGE_Y:
+		#return
+		
+	# Determinar comportamiento dependiendo del rango
+	if in_close_range(bat_pos, player_pos):
+		was_attack = true
+
+	if was_attack and in_attack_range(bat_pos, player_pos):
+		move_towards(Vector2(5, -5))
+	else:
+		was_attack = false
+		move_towards_player(player_pos, bat_pos)
+	
+
+func in_close_range(bat_pos, player_pos):
+	return bat_pos.x <= (player_pos.x + CLOSE_RANGE_X) and bat_pos.y >= (player_pos.y - CLOSE_RANGE_Y)
+
+func in_attack_range(bat_pos, player_pos):
+	return bat_pos.x <= (player_pos.x + ATTACK_RANGE) and bat_pos.y <= (player_pos.y + ATTACK_RANGE)
+
+func move_towards(offset):
+	position += offset
+
+func move_towards_player(player_pos, bat_pos):
+	if bat_pos.x <= (player_pos.x + CLOSE_RANGE_X):
+		position.x += 2
+	elif bat_pos.x >= (player_pos.x + CLOSE_RANGE_X):
+		position.x -= 2
+
+	if bat_pos.y >= (player_pos.y - CLOSE_RANGE_Y):
+		position.y -= 2
+	elif bat_pos.y <= (player_pos.y - CLOSE_RANGE_Y):
+		position.y += 2
+			
 func update_limits():
 	var half_window_width = (get_viewport_rect().size.x/2) * player_camera.zoom.x
 	left_limit = player_camera.get_screen_center_position().x - half_window_width
 	right_limit = player_camera.get_screen_center_position().x + half_window_width
 	
 func move_enemy(delta):
+	anim.speed_scale = 0.5
 	position += direction * SPEED * delta
-
 	if position.x < left_limit:
-		direction = Vector2(1, 0) # Mueve a la derecha
-		anim.flip_h = true		
+		change_direction(Vector2(1, 0), true)
 	elif position.x > right_limit:
-		direction = Vector2(-1, 0) # Mueve a la izquierd
-		anim.flip_h = false
-		
-	anim.play("fly")
+		change_direction(Vector2(-1, 0), false)
+
+func change_direction(new_direction, flip_h):
+	direction = new_direction
+	anim.flip_h = flip_h
 	
-func _on_area_2d_area_entered(area):
-	if area.is_in_group("Sword"):
-		is_dead = true
-		anim.play("Death")
-		velocity.x = 0
-
-
 func _on_animated_sprite_2d_animation_finished():
 	if is_dead && anim.animation == "Death":
 		queue_free()		
@@ -73,11 +102,14 @@ func _on_animated_sprite_2d_animation_finished():
 func _on_area_ataque_body_entered(body):
 	if body.name == "player1":
 		is_area_player = true
-		#var direction_to_player = (player.position - position)
-		#if position != direction_to_player:
-			#while position.x != position.x:
-				#position.x += 100
-				#
-		#$AnimatedSprite2D.rotation = direction_to_player.angle()
-		#print("Dirección al jugador:", direction_to_player)
-		#print("Posición del murciélago:", position)
+
+
+func _on_area_ataque_area_entered(area):
+	if area.is_in_group("Sword"):
+		is_dead = true
+		anim.play("Death")
+		velocity.x = 0
+
+func _on_area_ataque_body_exited(body):
+	if body.name == "player1":
+		is_area_player = false
