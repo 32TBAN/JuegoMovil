ab40e7f6409b3d05d75b838c3d5d70443b776fad
Kevin Guanoluisa <estebanguanoluisa55@gmail.com>
2024-12-13 11:31:52 -0500

    new tilemap

--- scripts/characters/players/cap1.gd
+++ scripts/characters/players/cap1.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -410.0
+const JUMP_VELOCITY = -510.0
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 @onready var anim = $AnimatedSprite2D
